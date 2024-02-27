df2ded86fdf85d7a439150dfdc152c9f41ed0751
Sezal Saundarya <87928300+Sezalsaundarya11@users.noreply.github.com>
2024-02-27 16:18:43 +0530

    Updated Solution - GfG to GitHub

--- Medium/Kadane's Algorithm/kadanes-algorithm.cpp
+++ Medium/Kadane's Algorithm/kadanes-algorithm.cpp
@@ -12,15 +12,16 @@ class Solution{
     long long maxSubarraySum(int arr[], int n){
         
         // Your code here
-        long long sum=0, maxi=INT_MIN;
-        
+        long long sum=0;
+        long long maxi = arr[0];
         for(int i=0; i<n; i++){
             sum+=arr[i];
-            maxi= max(sum,maxi);
+            maxi = max(sum,maxi);
             if(sum<0){
                 sum=0;
             }
         }
+        
         return maxi;
         
     }
